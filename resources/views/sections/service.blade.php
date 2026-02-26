f3251083a7d0b3d76a816ddd7e98ca13b1cd1ec3
rochim1 <63306482+rochim1@users.noreply.github.com>
2026-02-26 23:11:59 +0700

    update layanan

--- resources/views/sections/service.blade.php
+++ resources/views/sections/service.blade.php
@@ -1,6 +1,6 @@
 <section id="service" class="services-3 sec-padding overflow-hidden">
-    <div class="container ct-container">
-        <div class="sec-intro mx-auto text-center">
+    <div class="container">
+        <div class="sec-intro mx-auto text-center mb-5">
             <span class="sub-title fadeInUp">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -10,115 +10,109 @@
                     viewBox="0 0 30 18"
                 >
                     <path
-                        fill="#fd5523"
+                        fill="#047857"
                         d="M8.125 15.429h-2.5a.62.62 0 0 1-.442-.189.65.65 0 0 1 0-.909.62.62 0 0 1 .442-.188h2.5c.166 0 .325.068.442.188a.65.65 0 0 1 0 .91.62.62 0 0 1-.442.188m20.313 0h-1.563a.62.62 0 0 1-.442-.189.65.65 0 0 1 0-.909.62.62 0 0 1 .442-.188h1.044l.842-4.618c-.011-2.364-1.973-4.382-4.386-4.382h-4.104l-1.989 9h3.593c.166 0 .325.068.442.188a.653.653 0 0 1 0 .91.62.62 0 0 1-.442.188H17.5a.61.61 0 0 1-.488-.242.65.65 0 0 1-.122-.543L19.162 4.36a.65.65 0 0 1 .22-.361.6.6 0 0 1 .39-.14h4.603C27.476 3.857 30 6.452 30 9.642l-.949 5.261a.65.65 0 0 1-.214.377.6.6 0 0 1-.4.148"
                     />
                     <path
-                        fill="#fd5523"
+                        fill="#047857"
                         d="M24.375 18c-1.722 0-3.125-1.441-3.125-3.214s1.403-3.215 3.125-3.215c1.723 0 3.125 1.442 3.125 3.215S26.098 18 24.375 18m0-5.143c-1.034 0-1.875.866-1.875 1.929s.841 1.928 1.875 1.928 1.875-.865 1.875-1.928-.841-1.929-1.875-1.929M10.625 18C8.903 18 7.5 16.559 7.5 14.786s1.403-3.215 3.125-3.215c1.723 0 3.125 1.442 3.125 3.215S12.348 18 10.625 18m0-5.143c-1.034 0-1.875.866-1.875 1.929s.841 1.928 1.875 1.928 1.875-.865 1.875-1.928c0-1.064-.841-1.929-1.875-1.929m-2.5-9h-5a.62.62 0 0 1-.442-.188.65.65 0 0 1 0-.91.62.62 0 0 1 .442-.188h5c.166 0 .325.068.442.189a.65.65 0 0 1 0 .909.62.62 0 0 1-.442.188m0 3.857h-6.25a.62.62 0 0 1-.442-.188.65.65 0 0 1 0-.91.62.62 0 0 1 .442-.187h6.25c.166 0 .325.067.442.188a.65.65 0 0 1 0 .909.62.62 0 0 1-.442.188m0 3.857h-7.5a.62.62 0 0 1-.442-.188.65.65 0 0 1 0-.909.62.62 0 0 1 .442-.188h7.5c.166 0 .325.067.442.188a.65.65 0 0 1 0 .91.62.62 0 0 1-.442.187"
                     />
                     <path
-                        fill="#fd5523"
+                        fill="#047857"
                         d="M17.5 15.429h-4.375a.62.62 0 0 1-.442-.189.65.65 0 0 1 0-.909.62.62 0 0 1 .442-.188H17l2.84-12.857H5.625a.62.62 0 0 1-.442-.189.65.65 0 0 1 0-.909A.62.62 0 0 1 5.625 0h15a.61.61 0 0 1 .488.241.65.65 0 0 1 .122.543L18.11 14.927a.65.65 0 0 1-.22.36.6.6 0 0 1-.39.142"
                     />
                 </svg>
                 Layanan Kami</span
             >
             <h2 class="sec-title">Layanan Pengiriman Profesional</h2>
+            <p class="sec-desc">
+                Kami menyediakan berbagai layanan pengiriman yang dapat disesuaikan
+                dengan kebutuhan bisnis dan personal Anda.
+            </p>
         </div>
-        <div class="d-flex justify-content-center gap-4 row">
-            <div class="col-xl-3 col-md-6">
-                <div
-                    class="service-entry bg-cover round"
-                    style="background-image: url(&quot;{{
-                        asset('assets/images/serv-truck.png')
-                    }}&quot;)"
-                >
-                    <div class="serv-txt rounded-4">
-                        <div class="serv-heading">
-                            <div class="u-wrapper text-center">
-                                <div class="icon-box">
-                                    <i class="fas fa-truck-moving"></i>
-                                </div>
-                            </div>
-                            <div
-                                class="d-flex align-items-center justify-content-between serv-title"
-                            >
-                                <h3 class="display-4">Pengiriman Darat ></h3>
-                                <span
-                                    ><i class="fa-solid fa-angles-right"></i
-                                ></span>
-                            </div>
-                        </div>
-                        <p>
-                            Pengiriman darat menggunakan kendaraan seperti truk
-                            atau kereta untuk mengangkut barang. Metode ini
-                            ekonomis dan efisien untuk jarak dekat hingga
-                            menengah.
-                        </p>
+
+        <div class="service-grid">
+            <!-- Pengiriman Darat -->
+            <div class="service-card-new">
+                <div class="service-card-img">
+                    <img
+                        src="{{ asset('assets/images/serv-truck.png') }}"
+                        alt="Pengiriman Darat"
+                        class="img-fluid"
+                    />
+                    <div class="service-card-badge">Populer</div>
+                </div>
+                <div class="service-card-body">
+                    <div class="service-card-icon">
+                        <i class="fas fa-truck-moving"></i>
                     </div>
+                    <h3>Pengiriman Darat</h3>
+                    <p>
+                        Pengiriman darat menggunakan armada truk dan kendaraan
+                        terpercaya. Solusi ekonomis dan efisien untuk pengiriman
+                        jarak dekat hingga menengah di seluruh Indonesia.
+                    </p>
+                    <ul class="service-features">
+                        <li><i class="fa fa-check"></i> Ekonomis & efisien</li>
+                        <li><i class="fa fa-check"></i> Jarak dekat - menengah</li>
+                        <li><i class="fa fa-check"></i> Door-to-door service</li>
+                    </ul>
                 </div>
             </div>
-            <div class="col-xl-3 col-md-6">
-                <div
-                    class="service-entry bg-cover round"
-                    style="background-image: url(&quot;{{
-                        asset('assets/images/serv-plane.png')
-                    }}&quot;)"
-                >
-                    <div class="serv-txt rounded-4">
-                        <div class="serv-heading">
-                            <div class="u-wrapper text-center">
-                                <div class="icon-box">
-                                    <i class="fas fa-plane"></i>
-                                </div>
-                            </div>
-                            <div
-                                class="d-flex align-items-center justify-content-between serv-title"
-                            >
-                                <h3 class="display-4">Pengiriman Udara ></h3>
-                                <span
-                                    ><i class="fa-solid fa-angles-right"></i
-                                ></span>
-                            </div>
-                        </div>
-                        <p>
-                            Pengiriman udara menggunakan pesawat terbang untuk
-                            mengangkut barang. Metode ini cepat dan efisien
-                            untuk pengiriman jarak jauh dan barang berharga.
-                        </p>
+
+            <!-- Pengiriman Udara -->
+            <div class="service-card-new">
+                <div class="service-card-img">
+                    <img
+                        src="{{ asset('assets/images/serv-plane.png') }}"
+                        alt="Pengiriman Udara"
+                        class="img-fluid"
+                    />
+                    <div class="service-card-badge">Tercepat</div>
+                </div>
+                <div class="service-card-body">
+                    <div class="service-card-icon">
+                        <i class="fas fa-plane"></i>
                     </div>
+                    <h3>Pengiriman Udara</h3>
+                    <p>
+                        Pengiriman via udara menggunakan pesawat kargo untuk
+                        kecepatan maksimal. Ideal untuk barang berharga dan
+                        pengiriman jarak jauh dengan waktu terbatas.
+                    </p>
+                    <ul class="service-features">
+                        <li><i class="fa fa-check"></i> Pengiriman tercepat</li>
+                        <li><i class="fa fa-check"></i> Jarak jauh & antar pulau</li>
+                        <li><i class="fa fa-check"></i> Barang berharga aman</li>
+                    </ul>
                 </div>
             </div>
-            <div class="col-xl-3 col-md-6">
-                <div
-                    class="service-entry bg-cover round"
-                    style="background-image: url(&quot;{{
-                        asset('assets/images/serv-ship.png')
-                    }}&quot;)"
-                >
-                    <div class="serv-txt rounded-4">
-                        <div class="serv-heading">
-                            <div class="u-wrapper text-center">
-                                <div class="icon-box">
-                                    <i class="fas fa-ship"></i>
-                                </div>
-                            </div>
-                            <div
-                                class="d-flex align-items-center justify-content-between serv-title"
-                            >
-                                <h3 class="display-4">Pengiriman Laut</h3>
-                                <span
-                                    ><i class="fa-solid fa-angles-right"></i
-                                ></span>
-                            </div>
-                        </div>
-                        <p>
-                            Pengiriman laut menggunakan kapal untuk mengangkut
-                            barang melalui jalur laut. Cocok untuk pengiriman
-                            besar dan perdagangan internasional.
-                        </p>
+
+            <!-- Pengiriman Laut -->
+            <div class="service-card-new">
+                <div class="service-card-img">
+                    <img
+                        src="{{ asset('assets/images/serv-ship.png') }}"
+                        alt="Pengiriman Laut"
+                        class="img-fluid"
+                    />
+                    <div class="service-card-badge">Muatan Besar</div>
+                </div>
+                <div class="service-card-body">
+                    <div class="service-card-icon">
+                        <i class="fas fa-ship"></i>
                     </div>
+                    <h3>Pengiriman Laut</h3>
+                    <p>
+                        Pengiriman laut menggunakan kapal kargo untuk muatan
+                        besar dan berat. Cocok untuk kebutuhan distribusi skala
+                        besar dan perdagangan antar pulau.
+                    </p>
+                    <ul class="service-features">
+                        <li><i class="fa fa-check"></i> Kapasitas besar</li>
+                        <li><i class="fa fa-check"></i> Antar pulau & internasional</li>
+                        <li><i class="fa fa-check"></i> Hemat untuk volume besar</li>
+                    </ul>
                 </div>
             </div>
         </div>
